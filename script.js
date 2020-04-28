e817e8347aad9d4e2f5c91cb9979706714ade49f
ICVRXS <calebhay@rocketmail.com>
2020-04-28 14:10:59 -0700

    adding initial files with starting code

---
+++ script.js
@@ -0,0 +1,8 @@
+//clock to display current day and time
+//blocks for entering text
+    //event listeners for clicks 
+    //ability for user to enter text on-screen
+    //save buttons which save the text to local storage
+//functionality to change the color of each block according to the time of day
+    //change css through js for each hourly interval
+    
