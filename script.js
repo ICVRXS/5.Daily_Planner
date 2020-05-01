3e5f5f7f1d077bb62298eeaf161ab703d045a35e
ICVRXS <calebhay@rocketmail.com>
2020-05-01 14:58:21 -0700

    set up local storage to save tasks entered in textareas as a string

--- script.js
+++ script.js
@@ -6,16 +6,20 @@
 //functionality to change the color of each block according to the time of day
     //change css through js for each hourly interval
 var currentDay = $("#currentDay");
-var saveBtn = $(".saveBtn");
 var myTask = $(".my-task");
 var date = moment().date();
 
-moment().format("dddd, MMMM Do YYYY");
-
-currentDay.append(date);
+var saveBtn = $(".saveBtn"),
+    tasks = {};
 
 saveBtn.on("click", function(){
     var buttonHour = $(this).data("hour");
     var taskInput = $("#task-"+buttonHour);
-    taskInput.val();
+    var taskValue = taskInput.val();
+    tasks["#task"+buttonHour] = taskValue;
+    localStorage.setItem("tasks",JSON.stringify(tasks));
 });
+
+moment().format("dddd, MMMM Do YYYY");
+
+currentDay.append(date);
